2984cf91234736ac98371e8a16fb4915d8af8990
Maxim Frolov <frolovmx@gmail.com>
2015-05-28 11:06:33 +0200

    test case further simplified

--- jdk8-NodeNotFoundException/src/main/java/frol/VerfuegbareTarifkalender.java
+++ jdk8-NodeNotFoundException/src/main/java/frol/VerfuegbareTarifkalender.java
@@ -1,111 +1,73 @@
 package frol;
 
-import java.util.List;
+class VerfuegbareTarifkalender {
 
-import static frol.VerfuegbareTarifkalender.MakeItEasy.a;
-import static frol.VerfuegbareTarifkalender.MakeItEasy.listOf;
-import static frol.VerfuegbareTarifkalender.MakeItEasy.make;
-import static frol.VerfuegbareTarifkalender.MakeItEasy.with;
-
-public class VerfuegbareTarifkalender {
-
-    private KalenderInstantiator Kalender;
-    private KalenderversionInstantiator Kalenderversion;
-    private KalenderdatensatzInstantiator Kalenderdatensatz;
-
-
-    private Object kalenderSommerWinter() {
-        return make(a(
-                with(KalenderInstantiator.kalenderversionen, listOf(
+    private Donor kalenderSommerWinter() {
+        return a(
+                with(null, listOf(
                         a(
-                                with(KalenderversionInstantiator.gueltigVon, (Object) null),
-                                with(KalenderversionInstantiator.kalenderdatensaetze, listOf(
+                                with(null, null),
+                                with(null, listOf(
                                         a(
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null)
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null)
                                         ),
                                         a(
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null)
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null)
                                         ),
                                         a(
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null)
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null)
                                         ),
                                         a(
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null),
-                                                with(KalenderdatensatzInstantiator.mittwoch, (Object) null)
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null),
+                                                with(null, null)
                                         )
                                 ))
                         )
                 ))
-        ));
+        );
     }
 
+    interface List<T> {}
+    interface Donor<T> {}
+    class PropertyValue<T, V> {}
 
-
-
-    public static class KalenderdatensatzInstantiator {
-        public static final Property<Object, Object> mittwoch = null;
+    static <T> Donor<T> a(PropertyValue<? super T, ?>... propertyProviders) {
+        return null;
     }
 
-    public static class KalenderInstantiator {
-        public static final Property<Object, List<Object>> kalenderversionen = null;
+    static <T,V,W extends V> PropertyValue<T,V> with(Object property, W value) {
+        return null;
     }
 
-    public static class KalenderversionInstantiator {
-        public static final Property<Object, Object> gueltigVon = null;
-        public static final Property<Object, List<Object>> kalenderdatensaetze = null;
+    static <T,V,W extends V> PropertyValue<T,V> with(Object property, Donor<W> valueDonor) {
+        return null;
     }
 
-
-    public interface Donor<T> {}
-    public class Maker<T> implements Donor<T> {}
-    public class Property<T, V> {}
-    public class PropertyValue<T, V> {}
-
-    public static class MakeItEasy
-    {
-        public static <T> Maker<T> a(PropertyValue<? super T, ?>... propertyProviders) {
-            return null;
-        }
-
-        public static <T,V,W extends V> PropertyValue<T,V> with(Property<T,V> property, W value) {
-            return null;
-        }
-
-        public static <T,V,W extends V> PropertyValue<T,V> with(Property<T,V> property, Donor<W> valueDonor) {
-            return null;
-        }
-
-        public static <T> T make(Maker<T> maker) {
-            return null;
-        }
-
-        public static <T> Donor<List<T>> listOf(Donor<? extends T>... donors) {
-            return null;
-        }
-
+    static <T> Donor<List<T>> listOf(Donor<? extends T>... donors) {
+        return null;
     }
 }
